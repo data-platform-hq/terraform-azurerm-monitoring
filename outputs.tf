0d7b4c6962bb66c04cf2f3a9742f43b27b5e1ee8
dmytro_velychko3 <you@example.com>
2023-01-25 17:28:29 +0200

    feat: add index output values

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "workbook_adf_id" {
-  value       = azurerm_application_insights_workbook.adf.id
+  value       = azurerm_application_insights_workbook.adf[0].id
   description = "Azure Workbook ADF Template ID"
 }
 
 output "workbook_databricks_id" {
-  value       = azurerm_application_insights_workbook.databricks.id
+  value       = azurerm_application_insights_workbook.databricks[0].id
   description = "Azure Workbook Databricks Template ID"
 }
 
 output "dashboard_adf_id" {
-  value       = azurerm_portal_dashboard.adf.id
+  value       = azurerm_portal_dashboard.adf[0].id
   description = "Azure Shared Dashboard ADF ID"
 }
 
 output "dashboard_databricks_id" {
-  value       = azurerm_portal_dashboard.databricks.id
+  value       = azurerm_portal_dashboard.databricks[0].id
   description = "Azure Shared Dashboard Databricks ID"
 }
